6ecfb2730f06a71715a25ed957bbe9087501bd0c
jpllopes96 <joaolacaia96@gmail.com>
2023-05-12 20:33:34 -0300

    Homolog ok

--- src/main/java/com/jpllopes/dslist/projections/GameMinProjection.java
+++ src/main/java/com/jpllopes/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.jpllopes.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
